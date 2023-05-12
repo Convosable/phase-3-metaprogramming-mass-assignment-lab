1d4e031db007c2e192ef58a74ec7daed7642f38a
Connor Vosberg <connorjv@gmail.com>
2023-05-12 15:36:00 -0700

    commit tree dec974081d766f3e0570db18fad464a35061981f

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-  # your code here
+
+    def initialize(attributes)
+      attributes.each do |key, value|
+          self.class.attr_accessor(key)
+          self.send("#{key}=", value)
+      end
+    end
+
 end
+
+
+def greeting(name, programming_language)
+  puts "Hello, #{name}"
+  puts "Welcome to the #{programming_language} curriculum"
+end
+
+puts greeting(programming_language: "Ruby", name: "Leroy Jenkins")
